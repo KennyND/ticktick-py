07a1a7bab5c7bf4b57a339ad7fc830797635ef9d
Kenny Duran <kennyduran@Kennys-MBP.lan>
2024-10-25 23:08:36 -0400

    Update firefox version; log response error

--- ticktick/api.py
+++ ticktick/api.py
@@ -17,8 +17,8 @@ class TickTickClient:
 
     INITIAL_BATCH_URL = BASE_URL + 'batch/check/0'
 
-    USER_AGENT = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:123.0) Gecko/20100101 Firefox/123.0"
-    X_DEVICE_ = '{"platform":"web","os":"OS X","device":"Firefox 123.0","name":"unofficial api!","version":4531,' \
+    USER_AGENT = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:131.0) Gecko/20100101 Firefox/123.0"
+    X_DEVICE_ = '{"platform":"web","os":"OS X","device":"Firefox 131.0","name":"unofficial api!","version":4531,' \
                 '"id":"6490' + secrets.token_hex(10) + '","channel":"website","campaign":"","websocket":""}'
 
     HEADERS = {'User-Agent': USER_AGENT,
@@ -118,6 +118,7 @@ class TickTickClient:
         Raises:
             RuntimeError: If the status code of the response was not 200.
         """
+        print(response)
         if response.status_code != 200:
             raise RuntimeError(error_message)
 
